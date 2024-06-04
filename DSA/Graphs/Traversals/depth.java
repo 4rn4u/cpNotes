80bdd2e9100a51f7569be07420647e16bee420b1
4rn4u <vilx00@protonmail.com>
2024-06-04 12:35:49 +0200

    breath

---
+++ DSA/Graphs/Traversals/depth.java
@@ -0,0 +1,25 @@
+public class depth {
+    protected int visited[];
+    protected int orderVisited;
+
+    public int[] toArrayDFS(){
+        int res[] = new int[numVertices()];
+        visited = new int[numVertices()];
+        orderVisited = 0;
+        for (int i = 0; i<numVertices(); i++){
+            if (visited[i] == 0) toArrayDFS(i,res);
+        }
+        return res;
+    }
+
+    protected void toArrayDFS(int v, int res[]){
+        res[orderVisited++] = v;
+        visited[v] = 1
+        ListPOI<Adjacent> l = adjacentsTo(v);
+        for (l.begin(); !l.isEnd(); l.next()){
+            int w = l.get().getTarget();
+            if (visited[w] == 0) toArrayDFS(w, res);
+        }
+    }
+
+}
